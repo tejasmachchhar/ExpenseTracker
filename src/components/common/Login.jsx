b0017aff37819e4ce9891dd600989e3815e1ad14
tejasmachchhar <tejasmachchhar0904@gmail.com>
2025-02-20 11:54:08 +0530

    t

--- src/components/common/Login.jsx
+++ src/components/common/Login.jsx
@@ -4,7 +4,7 @@ export const Login = () => {
   return (
     <div style={{width: '500px', color: 'red'}}>Ok
       <div style={{width: '100%', color: 'red'}}>Okk
-        <br style={{color: 'red', width: '100%'}} />
+        {/* <br style={{color: 'red', width: '100%'}} /> */}
       </div>
     </div>
   )
